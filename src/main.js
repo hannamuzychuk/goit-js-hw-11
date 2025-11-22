3d149a88d44cc40b8acfeced3c038ff2625d5e13
Hanna Muzychuk <hannamuzychuk@gmail.com>
2025-11-22 12:04:59 +0100

    install izitoast

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
+
 import { getImagesByQuery } from './js/pixabay-api';
 import {
     createGallery,
