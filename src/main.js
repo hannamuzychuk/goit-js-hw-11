a78d6ebc2bc0344a333fa8ffb83199d027f36a9c
Hanna Muzychuk <hannamuzychuk@gmail.com>
2025-11-22 12:00:22 +0100

    create function

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+    createGallery,
+    clearGallery,
+    showLoader,
+    hideLodar,
+} from './js/render-functions'
